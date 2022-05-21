5f9b860592edf147fb3d25466424f3c5dd8e8e33
couvq <quentincouvelaire@gmail.com>
2022-05-21 12:51:53 -0400

    setup initial refs for passage and question

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import { Fragment } from 'react';
 
 const App = () => {
 
+  // setup references and state hook
+  const passageRef = useRef(null);
+  const questionRef = useRef(null);
+
 
   return (
     <div>React App Build Using BERT TensorFlow.js Model</div>
