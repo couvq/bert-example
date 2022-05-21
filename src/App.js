98ab228cbdf51a36b5b9dce3b7f7fb31612c16df
couvq <quentincouvelaire@gmail.com>
2022-05-21 12:35:22 -0400

    imported dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import * as tf from "@tensorflow/tfjs";
+import * as qna from "@tensorflow-models/qna";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
+import { Fragment } from 'react';
+
 const App = () => {
 
 
